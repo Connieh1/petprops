bfc4c91867da83a22a93bc9d9ec06ecddc58ef88
github username <constance.hyman@mchsi.com>
2019-06-27 20:45:23 -0700

    Build routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   root "application#home"
   get "home", to: "application#home"
 
+  resources :users
+
+  resources :pets
+
+  resources :posts do
+    resources :pets, only: [:index, :show, :new]
+  end
+
  
 
 end
