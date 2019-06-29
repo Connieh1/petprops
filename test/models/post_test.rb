581e214d98fca86bea60223c00881d11d7ebd156
github username <constance.hyman@mchsi.com>
2019-06-28 21:38:26 -0700

    Add tests to models/post_test

---
+++ test/models/post_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class PostTest < ActiveSupport::TestCase
+  
+	# def setup
+	# 	@post = Post.new(name: "Flopsy", content: "Best.Dog.Evah!")
+	# end
+  
+ #  # test "the truth" do
+ #  #   assert true
+ #  # end
+
+ #  test "post should be valid" do
+ #    assert @post.valid?
+  # end
+end
