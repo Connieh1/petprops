f438815182458e76a83f483462e5b64aef4471c8
github username <constance.hyman@mchsi.com>
2019-07-10 14:21:56 -0700

    Make changes to Users navbar tab

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,21 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		binding.pry
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			session[:user_id] = user.id
+			flash[:success] = "You have successfully logged in"
+			redirect_to user
+		else
+			flash.now[:danger] = "There was a problem with your login information."
+			render "new"
+		end
 	end
 
 	def destroy
+		session[:user_id] = nil
+		flash[:success] = "You have logged out"
+		reidrect_to root_path
 	end
 end
