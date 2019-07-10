7f42fb8afa89c9a63aa2cdb6da3fa477d4c18afa
github username <constance.hyman@mchsi.com>
2019-07-10 10:16:17 -0700

    Build SessionsController and add login/logout actions

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
 
   resources :posts
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
 end
