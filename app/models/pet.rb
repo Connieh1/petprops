4e9bf8bd61d942ba004bdc6863c74253c4c4e2fd
github username <constance.hyman@mchsi.com>
2019-09-10 03:27:58 -0500

    Add search method to model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -6,10 +6,14 @@ class Pet < ApplicationRecord
 	validates :species, presence: true
 	validates :breed, presence: true
 
-	def top_three_pets
+	def self.search(search)
+  	where("name LIKE ? OR species LIKE ? OR breed LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%") 
+	end
+
+	def self.top_three_pets
   	pets = Pet.includes(:posts).limit(3)
   	pets.flat_map do |pet|
-    	pet.posts.count
+     pet.posts.count
   	end
 	end
 
